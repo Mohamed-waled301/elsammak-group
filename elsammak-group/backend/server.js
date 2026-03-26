a687f2a1b70dc4183e85f07363004e29703793ff
Mohamed Waled <waled9090mohamed@gmail.com>
2026-03-26 03:15:29 +0200

    final fix cors clean

--- elsammak-group/backend/server.js
+++ elsammak-group/backend/server.js
@@ -16,7 +16,9 @@ app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/locations', require('./routes/locationRoutes'));
 
 const PORT = process.env.PORT || 8080;
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
